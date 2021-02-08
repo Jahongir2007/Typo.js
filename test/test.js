141d527bc34247cfda4abac542e619bfefd5732d
Jahongir Sobirov <69193276+Jahongir2007@users.noreply.github.com>
2021-02-08 10:21:19 +0500

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,16 @@
+import { viewTag, viewClass, viewID, color, type, transform, dec, decLine, decStyle, decColor, align, alignLast, adjust, weight, variant, style, size, shadow, text, overflow, indent } from './typo.js';
+viewTag("p") = type("arial");
+viewTag("p") = color("red")
+viewTag("p") = align("center");
+viewTag("p") = size("x-large");
+viewTag("p") = variant("small-caps");
+viewClass("demo") = type("arial");
+viewClass("demo") = color("red")
+viewClass("demo") = align("center");
+viewClass("demo") = size("x-large");
+viewClass("demo") = variant("small-caps");
+viewID("demo") = type("arial");
+viewID("demo") = color("red")
+viewID("demo") = align("center");
+viewID("demo") = size("x-large");
+viewID("demo") = variant("small-caps");
